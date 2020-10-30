95b9586c3ad77db9d7a076ceea9d317d2ee6861b
shema102 <sergeishemshur@gmail.com>
2020-10-30 11:16:44 +0200

    Added oled init

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import bluetooth_demo
+# import bluetooth_demo
 
-bluetooth_demo.demo()
+# bluetooth_demo.demo()
 
+from ssd1306 import SSD1306_I2C
+from machine import I2C, Pin
+import time
+
+sdaPin = Pin(21, Pin.OUT)
+sclPin = Pin(22, Pin.OUT)
+
+i2c = I2C(scl=sclPin, sda=sdaPin)
+oled = SSD1306_I2C(128, 64, i2c)
