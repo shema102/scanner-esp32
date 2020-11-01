4522651774199abad4987cbfdfad24e68a0f8af4
shema102 <sergeishemshur@gmail.com>
2020-11-01 14:18:35 +0200

    Added web repl

--- src/boot.py
+++ src/boot.py
@@ -1,5 +1,20 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 # import esp
 # esp.osdebug(None)
-# import webrepl
-# webrepl.start()
+
+import json
+import webrepl
+import network
+
+# Reading settings from file
+with open("config.json") as f:
+    config = json.load(f)
+ssid = config["ssid"]
+password = config["password"]
+
+# Connecting to wifi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+webrepl.start()
